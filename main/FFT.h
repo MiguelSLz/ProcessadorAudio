416e14a6bc0d70aa7ca8eb32cd80635f858503f4
SjLeonardo-Hub <leonardobiten@gmail.com>
2024-11-28 20:42:26 -0300

    Comments

--- main/FFT.h
+++ main/FFT.h
@@ -6,6 +6,18 @@
 class FFT{
 
 public:
+
+    /*
+        Ideias e sugestoes:
+
+        Vc entende melhor como a FFT funciona exatamente, mas poderiamos ter funcoes tipo: 
+        getFrequency();
+        inverseFFT();
+
+        acho que alem dos metodos podemos definir que a FFT tenha um atributo como:
+        float espectroFrequencia[1024];
+        para manipular e acessar... ai' podemos acessar esse buffer com um ponteiro la na classe Efeitos...
+    */
     FFT(); 
 };
 
