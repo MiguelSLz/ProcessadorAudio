e51c40cbc908a5e93fb9fc36c021e7485177c56e
SjLeonardo-Hub <leonardobiten@gmail.com>
2024-11-30 10:27:46 -0300

    Método setupADC() adicionado

--- main/Dados.cpp
+++ main/Dados.cpp
@@ -4,15 +4,17 @@
 #include "hal/adc_types.h"
 #include "driver/adc.h"
 #include <stdint.h>
- 
-void Dados::LerMic(){ 			// talvez seja interessante separar a parte do codigo antes do while
+
+void Dados::setupADC(){
 	Interrupts Interruptor;
 	Interruptor.configTimer();
 	
 	// Configurando o ADC
 	adc1_config_width(ADC_WIDTH_BIT_12);  // palavra de processamento do ADC
 	adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_12);
-	
+}
+
+void Dados::LerMic(){
 	unsigned int NumAmostras=0;
 	// escolha do while true com o break ao inves do for foi para apenas contar o numero de amostras quando for lido uma amostra (acontece a cada 125ms)
 	while(true){ // NumAmostras indo ate 65536 porque eh igual a 2^16, resultando na potencia de 2 mais proxima que resulte em 8 segundos de audio
