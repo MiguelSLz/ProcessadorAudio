703d80f9cc1a492be02bec86a4d588caee118619
SjLeonardo-Hub <leonardobiten@gmail.com>
2024-12-02 13:46:08 -0300

    Update Audio.h

--- main/Audio.h
+++ main/Audio.h
@@ -1,13 +1,16 @@
-#ifndef Effects_H
-#define Effects_H
-#include "Dados.h"
+#ifndef Audio_H
+#define Audio_H
+#include "Dados.cpp"
+#include "FFT.cpp"
 
 class Audio{
-
+    Dados data;
+    FFT frequencia;
 public:
-    Audio();
-    void EqualizerEffect(float *Sinal, char Volume);
-    void ECOEffect(float *Sinal,char Volume);
+    void gravar(unsigned char escolha, char forca, char volume[]);
+    void aplicarEco(char forca);
+    void aplicarEq(char volume[]);
+    void reproduzirAudio();
 };
 
-#endif // Effects_H
+#endif // Audio_H
