6980b8a2f44bb8f5958d2f7bebf1fbdc44eacbdd
SjLeonardo-Hub <leonardobiten@gmail.com>
2024-12-04 21:08:57 -0300

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,17 +1,18 @@
 
-#include "soc/soc.h" // Define a macro REG_WRITE
-
-
-
-
+Interface LCD;
 extern "C" {
 	void app_main(void);
 }
-
 void app_main(void){
+	LCD.runLcdUi();
+}
 
- 	while(1){
-	 
- 	}
- 
+static void botaoEsqInterrupt(void* arg){
+	LCD.setEsq(true);
+}
+static void botaoDirInterrupt(void* arg){
+	LCD.setDir(true);
+}
+static void botaoOKInterrupt(void* arg){
+	LCD.setOK(true);
 }
